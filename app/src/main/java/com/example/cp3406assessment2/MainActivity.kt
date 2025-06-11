5adb15aa7c9c01ecd7eeb510b61d796e1f507413
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2025-06-11 14:42:01 +1000

    Add top bar, bottom bar, floating button and custom theme

--- app/src/main/java/com/example/cp3406assessment2/MainActivity.kt
+++ app/src/main/java/com/example/cp3406assessment2/MainActivity.kt
@@ -4,35 +4,49 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.cp3406assessment2.model.Book
-import com.example.cp3406assessment2.ui.theme.CP3406Assessment2Theme
+import com.example.cp3406assessment2.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CP3406Assessment2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            AppTheme {
+                Scaffold(
+                    topBar = { TopBar() },
+                    bottomBar = { BottomBar() },
+                    floatingActionButton = { LogButton() },
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    innerPadding ->
                     Shelf(
-                        books = demoBooks(),
-//                        books = findFinishedBooks(demoBooks()),
-//                        books = findUnfinishedBooks(demoBooks()),
-//                        books = findUnreadBooks(demoBooks()),
-//                        books = demoBooks().sortedBy { it.title },
-//                        books = demoBooks().sortedBy { it.author },
-//                        books = demoBooks().sortedBy{ calculatePercentageRead(it) },
-                        modifier = Modifier.padding(innerPadding)
+                        books = demoBooks().sortedBy{ calculatePercentageRead(it) },
+                        modifier = Modifier.padding(innerPadding),
                     )
                 }
             }
@@ -92,12 +106,80 @@ fun calculatePercentageRead(book: Book): Double {
     return (book.readPageCount.toDouble() / book.totalPageCount.toDouble()) * 100
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(modifier: Modifier = Modifier) {
+    TopAppBar(
+        title = { Text("Bookshelf App") },
+        actions = {
+            IconButton(
+                onClick = { TODO() }
+            ) {
+                Icon(
+                    Icons.Default.Settings,
+                    contentDescription = "Settings button"
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun BottomBar(modifier: Modifier = Modifier) {
+    BottomAppBar {
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 56.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            IconButton(
+                onClick = { TODO() }
+            ) {
+                Icon(
+                    Icons.Default.Home,
+                    contentDescription = "Home button"
+                )
+            }
+            IconButton(
+                onClick = { TODO() }
+            ) {
+                Icon(
+                    Icons.Default.Search,
+                    contentDescription = "Search button"
+                )
+            }
+            IconButton(
+                onClick = { TODO() }
+            ) {
+                Icon(
+                    Icons.AutoMirrored.Filled.List,
+                    contentDescription = "Shelf button"
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun LogButton(modifier: Modifier = Modifier) {
+    ExtendedFloatingActionButton(
+        modifier = Modifier.padding(2.dp),
+        onClick = { TODO() }
+    ) {
+        Icon(
+            imageVector = Icons.Default.Edit,
+            contentDescription = "New Book button"
+        )
+        Text(
+            modifier = Modifier.padding(horizontal = 8.dp),
+            text = "New Book"
+        )
+    }
+}
+
 @Composable
 fun BookCard(book: Book, modifier: Modifier = Modifier) {
     Card(
-        modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth()
+        modifier = Modifier.padding(8.dp).fillMaxWidth()
     ) {
         Text(
             text = "${book.title} (${book.year})\nby ${book.author}" +
