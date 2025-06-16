1d874a23e5417dd09df3ba358e4c1cf98af0b750
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2025-06-16 10:50:54 +1000

    Update app to better reflect architecture shown in lecture videos

---
+++ app/src/main/java/com/example/cp3406assessment2/data/Book.kt
@@ -0,0 +1,11 @@
+package com.example.cp3406assessment2.data
+
+data class Book(
+    val title: String = "Title",
+    val author: String = "Author",
+    val genre: String = "Genre",
+    val year: Int = 1900,
+    val totalPageCount: Int = 0,
+    var readPageCount: Int = 0,
+    var rating: Int = 0
+)
