b5a399acb019f3085f30d6d633306eed0a49887d
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2025-07-09 21:06:11 +1000

    Add search button

--- app/src/main/java/com/example/cp3406assessment2/ui/SearchScreen.kt
+++ app/src/main/java/com/example/cp3406assessment2/ui/SearchScreen.kt
@@ -1,13 +1,17 @@
 package com.example.cp3406assessment2.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -54,44 +58,61 @@ fun SearchScreen(
             modifier = Modifier.padding(16.dp).fillMaxWidth()
         )
 
-        Box(
-            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Button(
-                onClick = { isExpanded = !isExpanded }
-            ) {
-                Icon(
-                    Icons.Default.Edit,
-                    contentDescription = "Filter button"
-                )
-                Text(
-                    text = "Search by $filter",
-                    modifier = Modifier.padding(horizontal = 8.dp)
-                )
+            Box {
+                Button(
+                    onClick = { isExpanded = !isExpanded }
+                ) {
+                    Text(
+                        text = "Search by: $filter",
+                        modifier = Modifier.padding(horizontal = 8.dp)
+                    )
+                    Icon(
+                        Icons.Default.KeyboardArrowDown,
+                        contentDescription = "Filter button"
+                    )
+                }
+
+                DropdownMenu(
+                    expanded = isExpanded,
+                    onDismissRequest = { isExpanded = false },
+                    modifier = Modifier.fillMaxWidth(0.4f)
+                ) {
+                    DropdownMenuItem(
+                        text = { Text("Title") },
+                        onClick = { filter = "title" }
+                    )
+
+                    DropdownMenuItem(
+                        text = { Text("Author") },
+                        onClick = { filter = "author" }
+                    )
+
+                    DropdownMenuItem(
+                        text = { Text("Genre") },
+                        onClick = { filter = "genre" }
+                    )
+
+                    DropdownMenuItem(
+                        text = { Text("Year") },
+                        onClick = { filter = "year" }
+                    )
+                }
             }
 
-            DropdownMenu(
-                expanded = isExpanded,
-                onDismissRequest = { isExpanded = false }
+            Button(
+                onClick = { onSearch(query, filter) }
             ) {
-                DropdownMenuItem(
-                    text = { Text("Title") },
-                    onClick = { filter = "title" }
+                Text(
+                    text = "Search",
+                    modifier = Modifier.padding(horizontal = 8.dp)
                 )
-
-                DropdownMenuItem(
-                    text = { Text("Author") },
-                    onClick = { filter = "author" }
-                )
-
-                DropdownMenuItem(
-                    text = { Text("Genre") },
-                    onClick = { filter = "genre" }
-                )
-
-                DropdownMenuItem(
-                    text = { Text("Year") },
-                    onClick = { filter = "year" }
+                Icon(
+                    Icons.Default.Search,
+                    contentDescription = "Search button"
                 )
             }
         }
