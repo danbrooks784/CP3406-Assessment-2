c1c92bc5dbdbc9f266530393dbc448b976b6a4a3
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2025-07-10 17:35:37 +1000

    Add edit book button

--- app/src/main/java/com/example/cp3406assessment2/ui/ShelfScreen.kt
+++ app/src/main/java/com/example/cp3406assessment2/ui/ShelfScreen.kt
@@ -1,12 +1,19 @@
 package com.example.cp3406assessment2.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.cp3406assessment2.data.Book
@@ -14,6 +21,7 @@ import com.example.cp3406assessment2.data.Book
 @Composable
 fun ShelfScreen(
     books: List<Book>,
+    onEditButtonPressed: (Book) -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column {
@@ -26,15 +34,27 @@ fun ShelfScreen(
                         .padding(8.dp)
                         .fillMaxWidth()
                 ) {
-                    Text(
-                        text = "${books[index].title} (${books[index].year})" +
-                                "\nby ${books[index].author}" +
-                                "\nGenre: ${books[index].genre}" +
-                                "\nPages read: ${books[index].readPageCount} / ${books[index].totalPageCount} " +
-                                "(${((books[index].readPageCount.toDouble() / books[index].totalPageCount.toDouble()) * 100).toInt()}%)" +
-                                "\nRating: ${books[index].rating} / 5",
-                        modifier = Modifier.padding(16.dp)
-                    )
+                    Row {
+                        Text(
+                            text = "${books[index].title} (${books[index].year})" +
+                                    "\nby ${books[index].author}" +
+                                    "\nGenre: ${books[index].genre}" +
+                                    "\nPages read: ${books[index].readPageCount} / ${books[index].totalPageCount} " +
+                                    "(${((books[index].readPageCount.toDouble() / books[index].totalPageCount.toDouble()) * 100).toInt()}%)" +
+                                    "\nRating: ${books[index].rating} / 5",
+                            modifier = Modifier.padding(16.dp).fillMaxWidth(0.7f)
+                        )
+
+                        Button(
+                            onClick = { onEditButtonPressed(books[index]) },
+                            modifier = Modifier.padding(16.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Edit,
+                                contentDescription = "Edit button"
+                            )
+                        }
+                    }
                 }
             }
         }
