7f3ce9627a1033973db801237b93d5e2061c4014
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2025-07-14 19:43:15 +1000

    Update view model to access and modify ui state, and receive books through book repository module

--- app/src/main/java/com/example/cp3406assessment2/viewmodel/BookViewModel.kt
+++ app/src/main/java/com/example/cp3406assessment2/viewmodel/BookViewModel.kt
@@ -1,13 +1,41 @@
 package com.example.cp3406assessment2.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.cp3406assessment2.data.Book
 import com.example.cp3406assessment2.data.BookRepository
+import com.example.cp3406assessment2.data.NetworkResult
+import com.example.cp3406assessment2.view.BookUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+/*
+    TODO: Update book repository instead of ui state!
+ */
 
 class BookViewModel(
     private val bookRepository: BookRepository
 ) : ViewModel() {
-    val books = bookRepository.getBooks()
+    val bookUiState = MutableStateFlow(BookUiState())
+
+    private fun getBooks() {
+        bookUiState.value = BookUiState(isLoading = true)
+        viewModelScope.launch {
+            when (val result = bookRepository.getBooks()) {
+                is NetworkResult.Success -> {
+                    bookUiState.update {
+                        it.copy(isLoading = false, books = result.data)
+                    }
+                }
+                is NetworkResult.Error -> {
+                    bookUiState.update {
+                        it.copy(isLoading = false, error = result.error)
+                    }
+                }
+            }
+        }
+    }
 
     lateinit var search: String
     lateinit var searchType: String
@@ -15,15 +43,23 @@ class BookViewModel(
     lateinit var bookToEdit: Book
 
     fun addBook(book: Book) {
-        books.add(book)
+        bookUiState.value.books.add(book)
     }
 
     fun deleteBook(book: Book) {
-        books.remove(book)
+        bookUiState.value.books.remove(book)
+    }
+
+    fun retrieveBooks(): List<Book> {
+        return bookUiState.value.books
     }
 
     fun searchQuery(query: String, filter: String) {
         search = query
         searchType = filter
+    }
+
+    init {
+        getBooks()
     }
 }
