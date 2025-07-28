833017f76d6a5533a9a99b0d3f3a2afb765571fb
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2025-07-28 18:14:33 +1000

    Implement search and unfinished books recommendation to home screen

--- app/src/main/java/com/example/cp3406assessment2/ui/views/HomeScreen.kt
+++ app/src/main/java/com/example/cp3406assessment2/ui/views/HomeScreen.kt
@@ -1,30 +1,145 @@
 package com.example.cp3406assessment2.ui.views
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.cp3406assessment2.data.Book
+import com.example.cp3406assessment2.ui.state.HomeUiState
+import com.example.cp3406assessment2.viewmodel.HomeViewModel
+import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
+    navigateToHome: () -> Unit,
+    navigateToSearch: () -> Unit,
+    navigateToShelf: () -> Unit,
+    onEditButtonPressed: (Book) -> Unit,
     onSearchButtonPressed: () -> Unit
 ) {
-    Scaffold(
-        topBar = {
-            BookTopAppBar() { }
-        },
+    val viewModel: HomeViewModel = koinViewModel()
+    val uiState: HomeUiState by viewModel.homeUiState.collectAsStateWithLifecycle()
 
+    Scaffold(
         bottomBar = {
-            BookBottomAppBar(onSearchButtonPressed)
+            BottomAppBar {
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 56.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(
+                        onClick = { navigateToHome() }
+                    ) {
+                        Icon(
+                            Icons.Default.Home,
+                            contentDescription = "Home button"
+                        )
+                    }
+                    IconButton(
+                        onClick = { navigateToSearch() }
+                    ) {
+                        Icon(
+                            Icons.Default.Search,
+                            contentDescription = "Search button"
+                        )
+                    }
+                    IconButton(
+                        onClick = { navigateToShelf() }
+                    ) {
+                        Icon(
+                            Icons.AutoMirrored.Filled.List,
+                            contentDescription = "Shelf button"
+                        )
+                    }
+                }
+            }
         },
 
         modifier = Modifier.fillMaxSize()
 
     ) { innerPadding ->
-    Text(
-        text = "Home screen",
-        modifier = Modifier.padding(innerPadding))
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            Text(
+                text = "Welcome back.",
+                fontSize = 40.sp,
+                modifier = Modifier.padding(16.dp).align(alignment = Alignment.CenterHorizontally)
+            )
+            Text(
+                text = "Search for new books to read:",
+                fontSize = 20.sp,
+                modifier = Modifier.padding(8.dp).align(alignment = Alignment.CenterHorizontally)
+            )
+            Button(
+                onClick = { onSearchButtonPressed() },
+                modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+            ) {
+                Text("Go to search")
+            }
+
+            if (uiState.unfinishedBooks.isEmpty()) {
+                Text("You have no unread books! You can find more books to read in the search menu.")
+            } else {
+                Text(
+                    text = "Or pick up an unfinished book:",
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(top = 32.dp, bottom = 16.dp)
+                        .align(alignment = Alignment.CenterHorizontally)
+                )
+
+                LazyColumn(
+                    modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+                ) {
+                    items(uiState.unfinishedBooks) { book ->
+                        Card(
+                            modifier = Modifier.padding(8.dp).fillMaxWidth()
+                        ) {
+                            Row {
+                                Text(
+                                    text = viewModel.displayBookInfo(book),
+                                    modifier = Modifier.padding(16.dp).fillMaxWidth(0.7f)
+                                )
+                                Button(
+                                    onClick = { onEditButtonPressed(book) },
+                                    modifier = Modifier.padding(16.dp).align(Alignment.CenterVertically)
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Default.Edit,
+                                        contentDescription = "Edit button"
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/cp3406assessment2/viewmodel/HomeViewModel.kt
@@ -0,0 +1,49 @@
+package com.example.cp3406assessment2.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cp3406assessment2.data.Book
+import com.example.cp3406assessment2.data.BookRepository
+import com.example.cp3406assessment2.ui.state.HomeUiState
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel(
+    private val bookRepository: BookRepository
+) : ViewModel() {
+    val homeUiState: StateFlow<HomeUiState> =
+        bookRepository.getUnfinishedBooks()
+            .map {
+                HomeUiState(unfinishedBooks = it)
+            }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5000),
+                initialValue = HomeUiState()
+            )
+
+    fun displayBookInfo(book: Book): String {
+        return "${book.title} (${book.year.take(4)})" +
+                "\nby ${
+                    if (book.authors.isEmpty()) "unknown author"
+                    else (book.authors.joinToString(separator = ", "))
+                }" +
+                "\nPages read: ${book.readPageCount} / ${book.totalPageCount} " +
+                "(${((book.readPageCount.toDouble() / book.totalPageCount.toDouble()) * 100).toInt()}%)"
+    }
+
+//    fun getRandomFavouriteAuthor() {
+//        var highestRatedBooks: List<Book> = listOf()
+//        var favouriteAuthors: MutableList<String> = mutableListOf()
+//        viewModelScope.launch {
+//            bookRepository.getHighestRatedBooks().map { highestRatedBooks = it }
+//        }
+//        for (book in highestRatedBooks) {
+//            val author = book.authors.joinToString(separator = ", ")
+//            favouriteAuthors.add(author)
+//        }
+//        homeUiState.value.randomFavouriteAuthor = favouriteAuthors.random()
+//    }
+}
