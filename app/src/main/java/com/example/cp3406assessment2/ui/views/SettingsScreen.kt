4125e6155365d34eff429de8eb302af81c0b2ee0
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2025-07-28 18:17:33 +1000

    Remove setting screen

--- app/src/main/java/com/example/cp3406assessment2/ui/views/SettingsScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cp3406assessment2.ui.views
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun SettingsScreen() {
-    Text("Settings screen")
-}
