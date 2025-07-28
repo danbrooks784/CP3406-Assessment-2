cec16a41a3c12c23fe5228898cb9a6af7f332b41
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2025-07-28 18:20:10 +1000

    Fix EditBookScreen navigate back and pass in navigation controls to screens with a bottom nav bar

--- app/src/main/java/com/example/cp3406assessment2/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/cp3406assessment2/navigation/AppNavigation.kt
@@ -1,7 +1,6 @@
 package com.example.cp3406assessment2.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -10,9 +9,9 @@ import androidx.navigation.navArgument
 import com.example.cp3406assessment2.data.Book
 import com.example.cp3406assessment2.ui.views.EditBookScreen
 import com.example.cp3406assessment2.ui.views.HomeScreen
+import com.example.cp3406assessment2.ui.views.NewBookScreen
 import com.example.cp3406assessment2.ui.views.SearchResultScreen
 import com.example.cp3406assessment2.ui.views.SearchScreen
-import com.example.cp3406assessment2.ui.views.SettingsScreen
 import com.example.cp3406assessment2.ui.views.ShelfScreen
 import kotlinx.serialization.json.Json
 
@@ -21,16 +20,28 @@ fun AppNavigation() {
     val navController = rememberNavController()
     NavHost(
         navController = navController,
-        startDestination = Screens.ShelfScreen.route
+        startDestination = Screens.HomeScreen.route
     ) {
         composable(Screens.HomeScreen.route) {
             HomeScreen(
+                navigateToHome = { navController.navigate(Screens.HomeScreen.route) },
+                navigateToSearch = { navController.navigate(Screens.SearchScreen.route) },
+                navigateToShelf = { navController.navigate(Screens.ShelfScreen.route) },
+                onEditButtonPressed = {
+                    book: Book ->
+                    navController.navigate(
+                        "${Screens.EditBookScreen.route}/${Json.encodeToString(book)}"
+                    )
+                },
                 onSearchButtonPressed = { navController.navigate(Screens.SearchScreen.route) }
             )
         }
 
         composable(Screens.SearchScreen.route) {
             SearchScreen(
+                navigateToHome = { navController.navigate(Screens.HomeScreen.route) },
+                navigateToSearch = { navController.navigate(Screens.SearchScreen.route) },
+                navigateToShelf = { navController.navigate(Screens.ShelfScreen.route) },
                 onSearch = {
                     query: String ->
                     navController.navigate("${Screens.SearchResultScreen.route}/${query}")
@@ -46,17 +57,33 @@ fun AppNavigation() {
         ) {
             SearchResultScreen(
                 query = it.arguments?.getString("query").toString(),
+                navigateBack = { navController.popBackStack() },
                 onAddButtonPressed = {
                     book: Book ->
                     navController.navigate(
-                        "${Screens.EditBookScreen.route}/${Json.encodeToString(book)}"
+                        "${Screens.NewBookScreen.route}/${Json.encodeToString(book)}"
                     )
                 }
             )
         }
 
+        composable(
+            "${Screens.NewBookScreen.route}/{book}",
+            arguments = listOf(
+                navArgument("book") { type = NavType.StringType }
+            )
+        ) {
+            NewBookScreen(
+                book = Json.decodeFromString((it.arguments?.getString("book") ?: "")),
+                navigateBack = { navController.navigate(Screens.SearchScreen.route) }
+            )
+        }
+
         composable(Screens.ShelfScreen.route) {
             ShelfScreen(
+                navigateToHome = { navController.navigate(Screens.HomeScreen.route) },
+                navigateToSearch = { navController.navigate(Screens.SearchScreen.route) },
+                navigateToShelf = { navController.navigate(Screens.ShelfScreen.route) },
                 onEditButtonPressed = {
                     book: Book ->
                     navController.navigate(
@@ -74,17 +101,8 @@ fun AppNavigation() {
         ) {
             EditBookScreen(
                 book = Json.decodeFromString((it.arguments?.getString("book") ?: "")),
-                onSaveButtonPressed = {
-                    navController.popBackStack()
-                },
-                onDeleteButtonPressed = {
-                    navController.popBackStack()
-                }
+                navigateBack = { navController.navigate(Screens.ShelfScreen.route) }
             )
-        }
-
-        composable(Screens.SettingsScreen.route) {
-            SettingsScreen()
         }
     }
 }
